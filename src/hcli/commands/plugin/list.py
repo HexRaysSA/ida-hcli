ea9e772f06a0997e1f08d7ef06da8041d7f14e0f
Willi Ballenthin <wballenthin@hex-rays.com>
2025-08-21 18:23:22 +0200

    plugins: index files by IDA version and platform

--- src/hcli/commands/plugin/list.py
+++ src/hcli/commands/plugin/list.py
@@ -10,6 +10,7 @@ import rich_click as click
 import hcli.lib.ida.plugin.repo.github
 from hcli.lib.commands import async_command
 from hcli.lib.console import console
+from hcli.lib.ida.plugin import ALL_PLATFORMS
 
 logger = logging.getLogger(__name__)
 
@@ -32,11 +33,22 @@ async def list_plugins(ctx) -> None:
         assert isinstance(token, str)
 
         plugin_repo = hcli.lib.ida.plugin.repo.github.GithubPluginRepo(token)
-        for plugin in sorted(plugin_repo.get_plugins(), key=lambda p: p.name):
-            console.print(f"[green]{plugin.name}[/green]")
 
-            for version in plugin.versions:
-                console.print(f"  [yellow]{version.version}:[/yellow] {version.url}")
+        plugins = plugin_repo.get_plugins()
+
+        for plugin in sorted(plugins, key=lambda p: p.name):
+            console.print(f"[blue]{plugin.name}[/blue]")
+
+            for version, locations in plugin.locations_by_version.items():
+                console.print(f"  {version}:")
+                for location in locations:
+                    ida_versions_str = location.ida_versions if location.ida_versions != ">=0" else "all"
+                    locations_str = ", ".join(location.platforms) if location.platforms != ALL_PLATFORMS else "all"
+                    console.print(
+                        f"    IDA: [yellow]{ida_versions_str}[/yellow], platforms: [yellow]{locations_str}[/yellow]"
+                    )
+                    console.print(f"    {location.url}")
+                    console.print("")
 
     except Exception as e:
         logger.warning("error: %s", e, exc_info=True)
