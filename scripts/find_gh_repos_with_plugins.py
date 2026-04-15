1d9f3ac9395d2ade175ef1b6b3a5f7919ee7dab1
Willi Ballenthin <wballenthin@hex-rays.com>
2026-04-15 11:02:45 +0200

    find_gh_repos_with_plugins: debug logging and token from argv

--- scripts/find_gh_repos_with_plugins.py
+++ scripts/find_gh_repos_with_plugins.py
@@ -1,6 +1,10 @@
-import os
+import sys
+import logging
 
 from hcli.lib.ida.plugin.repo.github import find_github_repos_with_plugins
 
-for repo in sorted(find_github_repos_with_plugins(os.environ["GITHUB_TOKEN"])):
+
+logging.basicConfig(level=logging.DEBUG)
+
+for repo in sorted(find_github_repos_with_plugins(sys.argv[1])):
     print(repo)
